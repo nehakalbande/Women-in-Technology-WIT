44e2b19f0c88b952ad98f570e84154808ff48caa
Neha Kalbande <neha.leenaharsh@gmail.com>
2021-06-03 00:38:00 +0530

    Developed WIT Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import { Switch } from "react-router";
 import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import IndexPage from "./pages/index";
+import ScholarshipPage from "./pages/scholarship";
+import ResourcesPage from "./pages/resources";
+import OrganisationComponent from "./pages/organisation";
+import inspirations from "./pages/inspirations";
+import ArticlesPage from "./pages/articless";
+import infographics from "./pages/infographics";
+import CommunityPage from "./pages/community";
+import ContributingPage from "./pages/contributing";
+import GithubPage from "./pages/github";
+import BeginnersPage from "./pages/beginners";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+      <Switch>
+        <Route path="/" exact component={IndexPage} />
+        <Route path="/scholarship" exact component={ScholarshipPage} />
+        <Route path="/resources" exact component={ResourcesPage} />
+        <Route path="/organisation" exact component={OrganisationComponent} />
+        <Route path="/infographics" exact component={infographics} />
+        <Route path="/articless" exact component={ArticlesPage} />
+        <Route path="/community" exact component={CommunityPage} />
+        <Route path="/inspirations" exact component={inspirations} />
+        <Route path="/contributing" exact component={ContributingPage} />
+        <Route path="/github" exact component={GithubPage} />
+        <Route path="/beginners" exact component={BeginnersPage} />
+        </Switch>
+      <Footer />
+      </Router>
     </div>
   );
 }
