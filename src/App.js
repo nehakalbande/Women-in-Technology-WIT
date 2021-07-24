6dde6bc4315e29e249b087b1b24733126b6f927a
Neha Kalbande <neha.leenaharsh@gmail.com>
2021-07-24 14:16:03 +0530

    Events Page Added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import CommunityPage from "./pages/community";
 import ContributingPage from "./pages/contributing";
 import GithubPage from "./pages/github";
 import BeginnersPage from "./pages/beginners";
+import EventsPage from "./pages/events";
+
 
 function App() {
   return (
@@ -32,6 +34,7 @@ function App() {
         <Route path="/contributing" exact component={ContributingPage} />
         <Route path="/github" exact component={GithubPage} />
         <Route path="/beginners" exact component={BeginnersPage} />
+        <Route path="/events" exact component={EventsPage} />
         </Switch>
       <Footer />
       </Router>
